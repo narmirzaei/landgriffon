58835d4de4437bc2f47ca5e22318b2e11cb3e1fa
barbara-chaves <barbarabarcellos90@gmail.com>
2023-01-26 16:39:22 +0100

    Fix scenarios.cy

--- client/cypress/e2e/scenarios.cy.ts
+++ client/cypress/e2e/scenarios.cy.ts
@@ -32,23 +32,23 @@ afterEach(() => {
 });
 
 describe('Scenarios', () => {
-  // it('should be able to see the scenarios page', () => {
-  //   cy.url().should('contain', '/data/scenarios');
-  //   cy.get('h1').should('have.text', 'Manage scenarios data');
-  //   cy.log('Scenarios is selected in the menu bar');
-  //   cy.get('[data-testisactive="true"]').should('have.text', 'Scenarios');
-  // });
+  it('should be able to see the scenarios page', () => {
+    cy.url().should('contain', '/data/scenarios');
+    cy.get('h1').should('have.text', 'Manage scenarios data');
+    cy.log('Scenarios is selected in the menu bar');
+    cy.get('[data-testisactive="true"]').should('have.text', 'Scenarios');
+  });
 
-  // it('should be the same scenarios cards length than API', () => {
-  //   cy.get('[data-testid="scenario-card"]').should('have.length', 10);
-  // });
+  it('should be the same scenarios cards length than API', () => {
+    cy.get('[data-testid="scenario-card"]').should('have.length', 10);
+  });
 
-  // it('should every scenario have interventions', () => {
-  //   cy.get('[data-testid="scenario-card"]')
-  //     .first()
-  //     .find('[data-testid="scenario-interventions-item"]')
-  //     .should('have.length', 2);
-  // });
+  it('should every scenario have interventions', () => {
+    cy.get('[data-testid="scenario-card"]')
+      .first()
+      .find('[data-testid="scenario-interventions-item"]')
+      .should('have.length', 2);
+  });
 
   it('should allow user with permissions create new scenarios and come back', () => {
     cy.intercept('api/v1/users/me', { fixture: 'profiles/all-permissions' }).as('profile');
@@ -63,11 +63,11 @@ describe('Scenarios', () => {
       .should('contain', '/data/scenarios');
   });
 
-  // it('should not allow user without permissions to create a scenarios', () => {
-  //   cy.intercept('api/v1/users/me', { fixture: 'profiles/no-permissions' }).as('profile');
-  //   cy.wait('@profile');
-  //   cy.get('[data-testid="scenario-add-button"]').should('be.disabled');
-  // });
+  it('should not allow user without permissions to create a scenarios', () => {
+    cy.intercept('api/v1/users/me', { fixture: 'profiles/no-permissions' }).as('profile');
+    cy.wait('@profile');
+    cy.get('[data-testid="scenario-add-button"]').should('be.disabled');
+  });
 
   it('a user with permission removes a scenario succesfully', () => {
     cy.intercept('api/v1/users/me', { fixture: 'profiles/all-permissions' });
@@ -127,126 +127,126 @@ describe('Scenarios', () => {
       .should('be.disabled');
   });
 
-  // it('a user sorts scenarios alphabetically', () => {
-  //   cy.intercept('GET', '/api/v1/scenarios?*sort=title*', {
-  //     statusCode: 200,
-  //     fixture: 'scenario/filters/by-name-results',
-  //   }).as('fetchScenariosByName');
+  it('a user sorts scenarios alphabetically', () => {
+    cy.intercept('GET', '/api/v1/scenarios?*sort=title*', {
+      statusCode: 200,
+      fixture: 'scenario/filters/by-name-results',
+    }).as('fetchScenariosByName');
 
-  //   // ? selects the "Sort by name" option and click on it
-  //   cy.get('[data-testid="select-sort-scenario"]').click();
-  //   cy.get('[role="listbox"]').type('{downArrow}{downArrow}{enter}');
-  //   cy.wait('@fetchScenariosByName');
+    // ? selects the "Sort by name" option and click on it
+    cy.get('[data-testid="select-sort-scenario"]').click();
+    cy.get('[role="listbox"]').type('{downArrow}{downArrow}{enter}');
+    cy.wait('@fetchScenariosByName');
 
-  //   // ? checks the user updates acording to the sort selection
-  //   cy.url().should('contain', 'sortBy=title');
+    // ? checks the user updates acording to the sort selection
+    cy.url().should('contain', 'sortBy=title');
 
-  //   cy.get('[data-testid="scenario-card"]').should('have.length', 10);
+    cy.get('[data-testid="scenario-card"]').should('have.length', 10);
 
-  //   // ? checks the first scenario displayed contains is titled "Scenario A" which is,
-  //   // ? according to the mockup, the first matchup alphabetically speaking
-  //   cy.get('[data-testid="scenario-card"]')
-  //     .first()
-  //     .find('[data-testid="scenario-title"]')
-  //     .should('have.text', 'Scenario A');
-  // });
+    // ? checks the first scenario displayed contains is titled "Scenario A" which is,
+    // ? according to the mockup, the first matchup alphabetically speaking
+    cy.get('[data-testid="scenario-card"]')
+      .first()
+      .find('[data-testid="scenario-title"]')
+      .should('have.text', 'Scenario A');
+  });
 
-  // it('a user sorts scenarios by most recent', () => {
-  //   cy.intercept('GET', '/api/v1/scenarios?*sort=-updatedA*', {
-  //     statusCode: 200,
-  //     fixture: 'scenario/filters/most-recent-results',
-  //   });
+  it('a user sorts scenarios by most recent', () => {
+    cy.intercept('GET', '/api/v1/scenarios?*sort=-updatedA*', {
+      statusCode: 200,
+      fixture: 'scenario/filters/most-recent-results',
+    });
 
-  //   // ? selects the "Sort by most recent" option and click on it
-  //   cy.get('[data-testid="select-sort-scenario"]').click();
-  //   cy.get('[role="listbox"]').type('{downArrow}').type('{upArrow}').type('{enter}');
+    // ? selects the "Sort by most recent" option and click on it
+    cy.get('[data-testid="select-sort-scenario"]').click();
+    cy.get('[role="listbox"]').type('{downArrow}').type('{upArrow}').type('{enter}');
 
-  //   // ? checks the user updates acording to the sort selection
-  //   cy.url().should('contain', 'sortBy=-updatedAt');
+    // ? checks the user updates acording to the sort selection
+    cy.url().should('contain', 'sortBy=-updatedAt');
 
-  //   cy.get('[data-testid="scenario-card"]').should('have.length', 10);
+    cy.get('[data-testid="scenario-card"]').should('have.length', 10);
 
-  //   // ? checks the first scenario displayed contains is titled "Test: Change Rubber Location Cambodia" which is,
-  //   // ? according to the mockup, the most recent scenario updated
-  //   cy.get('[data-testid="scenario-card"]')
-  //     .first()
-  //     .find('[data-testid="scenario-title"]')
-  //     .should('have.text', 'Test: Change Rubber Location Cambodia');
-  // });
+    // ? checks the first scenario displayed contains is titled "Test: Change Rubber Location Cambodia" which is,
+    // ? according to the mockup, the most recent scenario updated
+    cy.get('[data-testid="scenario-card"]')
+      .first()
+      .find('[data-testid="scenario-title"]')
+      .should('have.text', 'Test: Change Rubber Location Cambodia');
+  });
 
-  // it('after setting a sort option, refreshing the page keeps the option set', () => {
-  //   cy.intercept('GET', '/api/v1/scenarios?*sort=-updatedAt*', {
-  //     statusCode: 200,
-  //     fixture: 'scenario/filters/most-recent-results',
-  //   });
+  it('after setting a sort option, refreshing the page keeps the option set', () => {
+    cy.intercept('GET', '/api/v1/scenarios?*sort=-updatedAt*', {
+      statusCode: 200,
+      fixture: 'scenario/filters/most-recent-results',
+    });
 
-  //   // ? visits the scenario index with a sorting filter already applied
-  //   cy.visit('/data/scenarios?sortBy=-updatedAt');
+    // ? visits the scenario index with a sorting filter already applied
+    cy.visit('/data/scenarios?sortBy=-updatedAt');
 
-  //   // ? checks the selector is set according to the URL params
-  //   cy.get('[data-testid="select-sort-scenario"]')
-  //     .find('button')
-  //     .should('have.text', 'Sort by most recent');
+    // ? checks the selector is set according to the URL params
+    cy.get('[data-testid="select-sort-scenario"]')
+      .find('button')
+      .should('have.text', 'Sort by most recent');
 
-  //   // ? checks the first scenario displayed contains is titled "Test: Change Rubber Location Cambodia" which is,
-  //   // ? according to the mockup, the most recent scenario updated
-  //   cy.get('[data-testid="scenario-card"]')
-  //     .first()
-  //     .find('[data-testid="scenario-title"]')
-  //     .should('have.text', 'Test: Change Rubber Location Cambodia');
-  // });
+    // ? checks the first scenario displayed contains is titled "Test: Change Rubber Location Cambodia" which is,
+    // ? according to the mockup, the most recent scenario updated
+    cy.get('[data-testid="scenario-card"]')
+      .first()
+      .find('[data-testid="scenario-title"]')
+      .should('have.text', 'Test: Change Rubber Location Cambodia');
+  });
 
-  // it('a user looks up scenario using the search', () => {
-  //   cy.intercept(
-  //     'GET',
-  //     '/api/v1/scenarios?disablePagination=true&sort=-updatedAt&search[title]=interventions',
-  //     {
-  //       statusCode: 200,
-  //       fixture: 'scenario/filters/search-results',
-  //     },
-  //   ).as('fetchScenariosAfterSearch');
+  it('a user looks up scenario using the search', () => {
+    cy.intercept(
+      'GET',
+      '/api/v1/scenarios?disablePagination=true&sort=-updatedAt&search[title]=interventions',
+      {
+        statusCode: 200,
+        fixture: 'scenario/filters/search-results',
+      },
+    ).as('fetchScenariosAfterSearch');
 
-  //   // ? types "intervention" in the search filter
-  //   cy.get('[data-testid="search-name-scenario"]').type('interventions');
+    // ? types "intervention" in the search filter
+    cy.get('[data-testid="search-name-scenario"]').type('interventions');
 
-  //   cy.wait('@fetchScenariosAfterSearch');
+    cy.wait('@fetchScenariosAfterSearch');
 
-  //   // ? the URL should update according to what was written in the search filter
-  //   cy.url().should('contain', 'search=interventions');
+    // ? the URL should update according to what was written in the search filter
+    cy.url().should('contain', 'search=interventions');
 
-  //   // ? checks there are 5 scenarios matching the query, according to the mockup
-  //   cy.get('[data-testid="scenario-card"]').should('have.length', 5);
+    // ? checks there are 5 scenarios matching the query, according to the mockup
+    cy.get('[data-testid="scenario-card"]').should('have.length', 5);
 
-  //   // ? checks the first scenario contains the title "Scenario with interventions", according to the mockup
-  //   cy.get('[data-testid="scenario-card"]')
-  //     .first()
-  //     .find('[data-testid="scenario-title"]')
-  //     .should('have.text', 'Scenario with interventions');
-  // });
+    // ? checks the first scenario contains the title "Scenario with interventions", according to the mockup
+    cy.get('[data-testid="scenario-card"]')
+      .first()
+      .find('[data-testid="scenario-title"]')
+      .should('have.text', 'Scenario with interventions');
+  });
 
-  // it('reloading the page with the search query param should filter the list automatically', () => {
-  //   cy.intercept(
-  //     'GET',
-  //     '/api/v1/scenarios?disablePagination=true&sort=-updatedAt&search[title]=interventions',
-  //     {
-  //       statusCode: 200,
-  //       fixture: 'scenario/filters/search-results',
-  //     },
-  //   );
+  it('reloading the page with the search query param should filter the list automatically', () => {
+    cy.intercept(
+      'GET',
+      '/api/v1/scenarios?disablePagination=true&sort=-updatedAt&search[title]=interventions',
+      {
+        statusCode: 200,
+        fixture: 'scenario/filters/search-results',
+      },
+    );
 
-  //   // ? visits the scenario index with a sorting filter already applied
-  //   cy.visit('/data/scenarios?search=interventions');
+    // ? visits the scenario index with a sorting filter already applied
+    cy.visit('/data/scenarios?search=interventions');
 
-  //   // ? checks the search filter is populated automatically with the value coming from URL params
-  //   cy.get('[data-testid="search-name-scenario"]').should('have.value', 'interventions');
+    // ? checks the search filter is populated automatically with the value coming from URL params
+    cy.get('[data-testid="search-name-scenario"]').should('have.value', 'interventions');
 
-  //   // ? checks there are 5 scenarios matching the query, according to the mockup
-  //   cy.get('[data-testid="scenario-card"]').should('have.length', 5);
+    // ? checks there are 5 scenarios matching the query, according to the mockup
+    cy.get('[data-testid="scenario-card"]').should('have.length', 5);
 
-  //   // ? checks the first scenario contains the title "Scenario with interventions", according to the mockup
-  //   cy.get('[data-testid="scenario-card"]')
-  //     .first()
-  //     .find('[data-testid="scenario-title"]')
-  //     .should('have.text', 'Scenario with interventions');
-  // });
+    // ? checks the first scenario contains the title "Scenario with interventions", according to the mockup
+    cy.get('[data-testid="scenario-card"]')
+      .first()
+      .find('[data-testid="scenario-title"]')
+      .should('have.text', 'Scenario with interventions');
+  });
 });
