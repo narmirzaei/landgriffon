73ca31186291c5041785e5a3b7b16ffa9237c49f
barbara-chaves <barbarabarcellos90@gmail.com>
2023-03-05 14:29:12 +0100

    Fix test

--- client/cypress/e2e/analysis.cy.ts
+++ client/cypress/e2e/analysis.cy.ts
@@ -15,7 +15,6 @@ afterEach(() => {
   cy.logout();
 });
 
-
 describe('Analysis tab', () => {
   beforeEach(() => {
     cy.visit('/analysis');
@@ -134,7 +133,43 @@ describe('Analysis filters', () => {
     cy.get('[data-testid="tree-select-materials-filter"]')
       .find('div[role="listbox"]')
       .find('.rc-tree-treenode:visible')
-      .should('have.length', 1); // first treenode is empty
+      .should('have.length', 1) // first treenode is empty
+      .click();
+
+    // Check that the location types endpoint is fetched with the selected params
+    cy.wait('@locationTypes').its('request.query').should('include.all.keys', 'materialIds', 'supplierIds', 'originIds');
+
+    // Apply filters
+    cy.get('[data-testid="more-filters-apply-btn"]').click();
+    cy.wait('@fetchImpactTable')
+      .its('request.query')
+      .should('include.all.keys', 'materialIds', 'supplierIds', 'originIds');
+
+    // Check metadata sentence
+    cy.get('[data-testid="analysis-dynamic-metadata"]')
+      .should('include.text', MATERIAL)
+      .should('include.text', COUNTRY)
+      .should('include.text', LOCATION)
+
+    // Remove filters
+    cy.get('[data-testid="more-filters-button"]').click({ force: true });
+    cy.get('[data-testid="tree-select-suppliers-filter"] button').click()
+    cy.get('[data-testid="tree-select-origins-filter"] button').click()
+    cy.get('[data-testid="tree-select-materials-filter"] button').click()
+
+    // Apply remove filters
+    cy.get('[data-testid="more-filters-apply-btn"]').click();
+
+    // Check the request was made without filters
+    cy.wait('@locationTypes')
+      .its('request.query')
+      .should('not.have.keys', 'materialIds', 'supplierIds', 'originIds')
+
+    // Check that the metadata sentence don't have the filters
+    cy.get('[data-testid="analysis-dynamic-metadata"]')
+      .should('not.include.text', MATERIAL)
+      .should('not.include.text', COUNTRY)
+      .should('not.include.text', LOCATION)
   });
 });
 
@@ -262,187 +297,6 @@ describe('Analysis table', () => {
         .should('have.value', toYear);
       // Check if the payload request is correct
       cy.wait('@fetchImpactTable').its('request.query.endYear').should('eql', toYear.toString());
-    });
-  });
-});
-
-describe('Analysis scenarios', () => {
-  it('users with "canCreateScenario" permission should be able to click add new scenario button', () => {
-    cy.intercept('/api/v1/users/me', { fixture: '/profiles/all-permissions.json' }).as('profile');
-    cy.visit('/analysis/map');
-    cy.wait('@profile');
-    cy.get('a[data-testid="create-scenario"]').click();
-    cy.wait('@profile');
-    cy.url().should('contain', '/data/scenarios/new');
-  });
-
-  it('users without "canCreateScenario" permission should not be able to click add new scenario button', () => {
-    cy.intercept('/api/v1/users/me', { fixture: '/profiles/no-permissions.json' });
-    cy.visit('/analysis/map');
-    cy.get('a[data-testid="create-scenario"]').should('not.exist');
-  });
-
-  it('should be scenarioIds empty when there is no scenario selected in the more filters endpoints', () => {
-    cy.intercept('GET', '/api/v1/**/trees?*').as('treesSelectors');
-    cy.visit('/analysis/table');
-    cy.wait('@treesSelectors').then((interception) => {
-      const url = new URL(interception.request.url);
-      const scenarioIds = url.searchParams.get('scenarioIds');
-      expect(scenarioIds).to.be.null;
-      expect(scenarioIds).to.be.null;
-    });
-  });
-
-  it('should be able to select a scenario vs actual data in the comparison select', () => {
-    cy.visit('/analysis/table');
-    cy.wait('@scenariosNoPaginated');
-
-    cy.intercept(
-      'GET',
-      '/api/v1/**/trees?withSourcingLocations=true&scenarioIds[]=8dfd0ce0-67b7-4f1d-be9c-41bc3ceafde7',
-    ).as('treesSelectorsWithScenarioId');
-
-    cy.get('[data-testid="scenario-item-null"]') // actual data
-      .find('[data-testid="select-comparison"]')
-      .click()
-      .find('input:visible')
-      .type('Test{enter}');
-
-    cy.url().should('contain', 'compareScenarioId=8dfd0ce0-67b7-4f1d-be9c-41bc3ceafde7');
-
-    // checking comparison cell is there
-    cy.wait('@scenarioVsActual')
-      .its('request.url')
-      .should('contain', 'comparedScenarioId=8dfd0ce0-67b7-4f1d-be9c-41bc3ceafde7');
-    cy.get('[data-testid="comparison-cell"]').should('have.length.above', 1);
-
-    // checking tree selectors on more filers
-    cy.wait('@treesSelectorsWithScenarioId')
-      .its('request.url')
-      .should('contain', 'scenarioIds[]=8dfd0ce0-67b7-4f1d-be9c-41bc3ceafde7');
-  });
-
-  it('should be able to select a scenario vs scenario in the comparison select', () => {
-    cy.intercept(
-      'GET',
-      '/api/v1/impact/table?*scenarioId=8dfd0ce0-67b7-4f1d-be9c-41bc3ceafde7*',
-    ).as('fetchImpactTableData');
-    cy.visit('/analysis/table');
-    cy.wait('@scenariosNoPaginated');
-    cy.wait('@scenariosList');
-
-    cy.get('[data-testid="scenario-item-8dfd0ce0-67b7-4f1d-be9c-41bc3ceafde7"]')
-      .find('[data-testid="scenario-item-radio"]')
-      .click();
-
-    // Check that the location types endpoint is fetched with the selected params
-    cy.wait('@locationTypes').its('request.query').should('include.all.keys', 'materialIds', 'supplierIds', 'originIds');
-
-    // Apply filters
-    cy.get('[data-testid="more-filters-apply-btn"]').click();
-    cy.wait('@fetchImpactTable')
-      .its('request.query')
-      .should('include.all.keys', 'materialIds', 'supplierIds', 'originIds');
-
-    // Check metadata sentence
-    cy.get('[data-testid="analysis-dynamic-metadata"]')
-      .should('include.text', MATERIAL)
-      .should('include.text', COUNTRY)
-      .should('include.text', LOCATION)
-
-    // Remove filters
-    cy.get('[data-testid="more-filters-button"]').click({ force: true });
-    cy.get('[data-testid="tree-select-suppliers-filter"] button').click()
-    cy.get('[data-testid="tree-select-origins-filter"] button').click()
-    cy.get('[data-testid="tree-select-materials-filter"] button').click()
-
-    // Apply remove filters
-    cy.get('[data-testid="more-filters-apply-btn"]').click();
-
-    // Check the request was made without filters
-    cy.wait('@locationTypes')
-      .its('request.query')
-      .should('not.have.keys', 'materialIds', 'supplierIds', 'originIds')
-    cy.wait('@fetchImpactTable')
-      .its('request.query')
-      .should('not.have.keys', 'materialIds', 'supplierIds', 'originIds')
-
-    // Check that the metadata sentence don't have the filters
-    cy.get('[data-testid="analysis-dynamic-metadata"]')
-      .should('not.include.text', MATERIAL)
-      .should('not.include.text', COUNTRY)
-      .should('not.include.text', LOCATION)
-  });
-});
-
-describe('Analysis contextual layers', () => {
-  beforeEach(() => {
-    cy.visit('/analysis/map');
-  });
-
-  it('requests should not include the params indicatorId and should be in resolution 4', () => {
-    cy.wait(['@fetchIndicators', '@fetchContextualLayerCategories']);
-
-    cy.get('[data-testid="contextual-layer-modal-toggle"]').click();
-    cy.get('[data-testid="category-header-Environmental datasets"]').click();
-    cy.get('[data-testid="layer-settings-item-Agriculture blue water footprint"]')
-      .find('[data-testid="switch-button"]')
-      .click();
-    cy.get('[data-testid="contextual-layer-apply-button"').click();
-
-    cy.wait('@fetchContextualLayerH3Data').then((interception) => {
-      expect(interception.request.url).not.to.contain('indicatorId');
-      expect(interception.request.url).contain('year');
-      expect(interception.request.url).contain('resolution=4');
-    });
-  });
-
-  it('materials requests should not include the params indicatorId and should be in resolution 4', () => {
-    cy.wait(['@fetchIndicators', '@fetchContextualLayerCategories']);
-
-    cy.get('[data-testid="contextual-layer-modal-toggle"]').click();
-    cy.get('[data-testid="contextual-material-header"]')
-      .click()
-      .find('[data-testid="switch-button"]')
-      .click();
-    cy.wait(100);
-    cy.get('[data-testid="contextual-material-content"]')
-      .find('[data-testid="tree-select-material"]')
-      .find('input[type="search"]')
-      .type('Cotton');
-    cy.get('[data-testid="tree-select-search-results"]').find('button').click();
-    cy.get('[data-testid="contextual-layer-apply-button"').click();
-
-    cy.wait('@fetchMaterialLayerH3Data').then((interception) => {
-      expect(interception.request.url).not.to.contain('indicatorId');
-      expect(interception.request.url).contain('year');
-      expect(interception.request.url).contain('resolution=4');
-    });
-  });
-});
-
-describe('Analysis charts', () => {
-  beforeEach(() => {
-    cy.visit('/analysis/chart');
-  });
-
-  it('should load one chart per indicator', () => {
-    cy.wait(['@fetchIndicators', '@fetchChartRanking']).then(() => {
-      cy.get('[data-testid="analysis-chart"]').as('chart');
-      cy.get('@chart').should('be.visible');
-      cy.get('@chart').find('.recharts-responsive-container').and('have.length', 5);
-    });
-  });
-});
-
-describe('Analysis table', () => {
-  beforeEach(() => {
-    cy.visit('/analysis/table');
-  });
-
-  it('should load the table', () => {
-    cy.wait(['@fetchIndicators', '@fetchImpactTable']).then(() => {
-      cy.get('[data-testid="analysis-table"]').should('be.visible');
     });
   });
 });
